5fde75a282221a594c32f711f1224a75224aac97
kaisei <me@kaisei.dev>
2021-08-14 18:29:06 +0900

    chore: change design button and input

--- src/components/ImageController.tsx
+++ src/components/ImageController.tsx
@@ -1,16 +1,31 @@
 import React, { useContext } from 'react';
+import { IconButton } from 'theme-ui';
 import { albumContext } from '../contexts/AlbumContext';
 
 export default function ImageFrame() {
   const ctx = useContext(albumContext);
   return (
     <div>
-      <button type="button" onClick={ctx.prev}>
-        Prev
-      </button>
-      <button type="button" onClick={ctx.next}>
-        Next
-      </button>
+      <IconButton type="button" onClick={ctx.prev}>
+        <svg viewBox="0 0 24 24" width="24" height="24" fill="currentcolor">
+          <polygon
+            points="23 0,1 12,23 24"
+            fill="none"
+            stroke="currentcolor"
+            strokeWidth={2}
+          />
+        </svg>
+      </IconButton>
+      <IconButton type="button" onClick={ctx.next}>
+        <svg viewBox="0 0 24 24" width="24" height="24" fill="currentcolor">
+          <polygon
+            points="1 0,23 12,1 24"
+            fill="none"
+            stroke="currentcolor"
+            strokeWidth={2}
+          />
+        </svg>
+      </IconButton>
     </div>
   );
 }
